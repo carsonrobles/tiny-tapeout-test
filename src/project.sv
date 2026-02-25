774fa263946263c44b3df3d625572861097ec63e
Carson Robles <144639615+carsonrobles13@users.noreply.github.com>
2026-02-24 22:30:25 -0800

    Add uart in loop back

--- src/project.sv
+++ src/project.sv
@@ -16,7 +16,29 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  logic [15:0] cnt;
+  wire       data_valid;
+  wire [7:0] data;
+
+  uart #(
+    .CLKS_PER_BIT ( 16 ) // TODO
+  ) u_uart (
+    .clk_i           ( clk   ),
+    .rst_ni          ( rst_n ),
+  
+    .en_i            ( 1'b1  ),
+  
+    .rx_data_valid_o ( data_valid ),
+    .rx_data_o       ( data       ),
+  
+    .tx_data_ready_o ( /* nc */   ),
+    .tx_data_valid_i ( data_valid ),
+    .tx_data_i       ( data       ),
+  
+    .rx_i            ( ui_in[0]   ),
+    .tx_o            ( uo_out[0]  )
+  );
+
+  logic [14:0] cnt;
 
   always_ff @(posedge clk) begin
     if (~rst_n)
@@ -26,8 +48,8 @@ module tt_um_example (
   end
 
   assign uio_oe = '1;
-  assign {uio_out, uo_out} = cnt;
+  assign {uio_out, uo_out[7:1]} = cnt;
 
-  wire _unused = &{1'b0, ui_in};
+  wire _unused = &{1'b0, ui_in[7:1]};
 
 endmodule
